aef7226be35dca8fc6b227932ae7e0ff21fa90d3
rulamon <ruben.lamon@gmail.com>
2019-01-26 20:31:45 +0100

    basic structure set up

---
+++ src/components/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class App extends React.component {
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            
+        )
+    }
+}
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
